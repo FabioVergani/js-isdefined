068e4794884999915dd70b667ec71dc1530fdee2
FabioVergani <fabio.vergani.1975@gmail.com>
2015-06-09 17:15:55 +0200

    Update isdefined.js

--- isdefined.js
+++ isdefined.js
@@ -1,4 +1,6 @@
 function isdefined(o){return o!=='' && typeof(o)!=='undefined';}
+function isDefined(e){return e!=='' && typeof(e)!=='undefined';}
+
 isdefined.array=function(oa){var o=oa;return isdefined(o) && Array.isArray(o);};
 
 
